1f1be332570612f2387842c0e24aeabc5d4b8bd7
alexaylwin <alexaylwin@gmail.com>
2013-02-28 22:41:57 -0500

    Added comments and readme

--- GenerateMatrixPermutations/GenerateMatrixPermuations.py
+++ GenerateMatrixPermutations/GenerateMatrixPermuations.py
@@ -1,3 +1,16 @@
+"""
+GenerateMatrixPermuations.py
+
+This script can be used to generate all the different permuations of a 2x2 matrix
+with all values within a range of MIN and MAX
+
+Feburary 2013
+Alex Aylwin
+
+"""
+
+#flattenMatrix - not used in this script, but this function
+#flattens a matrix into an array
 def flattenMatrix(matrix):
 	flatArray = []
 	size = len(matrix)
@@ -6,6 +19,8 @@ def flattenMatrix(matrix):
 			flatArray.append(matrix[i][j])
 	return flatArray
 
+#souffleArray - this function 'rises' an array into a square matrix,
+#dimensions size x size
 def souffleArray(array, size):
 	matrix = []
 	for i in range(size):
@@ -15,6 +30,14 @@ def souffleArray(array, size):
 		matrix.append(row)
 	return matrix
 
+#fill 4 - this function is the controller function, that creates the
+#matricies and calls an operator function on each of them. This other
+#function should have side effects that capture the results of whatever
+#calculation needs to be performed
+
+#To change the size of the matrix (ie, not 2x2) add for loops to match
+#the length of the ARRAY that would come out of flattening the matrix,
+#ie a 4x4 matrix should have 16 nested for loops.
 def fill4(min, max):
 	array = []
 	size = 2
@@ -26,8 +49,9 @@ def fill4(min, max):
 					matrix = souffleArray(array, size)
 					doFunctionOnMatrix(matrix)
 
+#This is the calculation function that actually does some work on the matricies
 def doFunctionOnMatrix(matrix):
 	print(matrix)
 
-
+#Kick off the calculation, with a range from 0 - 3, inclusive
 fill4(0,3)
